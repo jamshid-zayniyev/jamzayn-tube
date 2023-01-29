a730e0f8ca2eba7a76ceae961f25b409845a3289
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-01-29 17:55:03 +0500

    JHKJN
    JNKNK

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export  {default as App } from "./app/app"
+export  {default as Main } from "./main/main"
+export  {default as Channel } from "./channel/channel"
+export  {default as Search } from "./search/search"
+export  {default as Videodetail } from "./video-detail/video-detail"
+export {default as Navbar} from "./navbar/navbar"
